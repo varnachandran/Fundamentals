5022e85a1975e682c848e89c7d64befb47841978
Varna Chandran <varna417@gmail.com>
2018-08-18 19:40:03 +1000

    Added insert and search for BST

--- BST.py
+++ BST.py
@@ -10,15 +10,32 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        while self.root:
-            if self.root.value <= new_val:
-                self.root.left = Node(new_val)
-            else:
-                self.root.right=Node(new_val)
+        self.root = self.iter_insert(self.root,new_val)
 
 
     def search(self, find_val):
-        return False
+        return self.iter_search(self.root,find_val)
+
+    def iter_insert(self,start,value_to_insert):
+        if not start:
+            return Node(value_to_insert)
+        else:
+            if value_to_insert <= start.value:
+                start.left = self.iter_insert(start.left,value_to_insert)
+            else:
+                start.right = self.iter_insert(start.right,value_to_insert)
+        return start
+
+    def iter_search(self,start,value):
+        if start:
+            if start.value==value:
+                return True
+            elif value <= start.value:
+                return self.iter_search(start.left, value)
+            else:
+                return self.iter_search(start.right, value)
+        else:
+            return False
 
 
 # Set up tree
@@ -32,6 +49,6 @@ tree.insert(5)
 
 # Check search
 # Should be True
-print tree.search(4)
+print tree.search(1)
 # Should be False
 print tree.search(6)
