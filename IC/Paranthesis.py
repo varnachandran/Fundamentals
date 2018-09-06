efeafbdc650e96767761f8df863307f23f2f89b9
Varna Chandran <varna417@gmail.com>
2018-09-06 22:25:53 +1000

    Pranthesis index

---
+++ IC/Paranthesis.py
@@ -0,0 +1,39 @@
+import unittest
+from collections import deque
+
+
+def get_closing_paren(sentence, opening_paren_index):
+    # Find the position of the matching closing parenthesis
+    count_left = 0
+
+    for index in range(opening_paren_index, len(sentence)):
+        if sentence[index] == '(':
+            count_left += 1
+        elif sentence[index] == ')':
+            count_left -=1
+        if count_left==0:
+            return index
+
+    raise Exception('Unequal paranthesis')
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_all_openers_then_closers(self):
+        actual = get_closing_paren('((((()))))', 2)
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_mixed_openers_and_closers(self):
+        actual = get_closing_paren('()()((()()))', 5)
+        expected = 10
+        self.assertEqual(actual, expected)
+
+    def test_no_matching_closer(self):
+        with self.assertRaises(Exception):
+            get_closing_paren('()(()', 2)
+
+
+if __name__=='__main__':
+    unittest.main
